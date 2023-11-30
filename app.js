6aa5228e354bebba3c775598788a405473a89ab1
Madhu22 <madhuxsharma@gmail.com>
2023-11-30 15:32:38 -0800

    JSON usage

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const businessRouter = require('./routes/businessRouter');
 // const valueRouter = require('./routes/valueRouter');
 
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3001;
 
 app.use(
