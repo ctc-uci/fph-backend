9c8ebbdaadcb3e644a0166bd49163744e7001f47
Kainoa Nishida <kanishid@uci.edu>
2023-12-04 16:23:10 -0800

    sheesh

--- routes/donationRouter.js
+++ routes/donationRouter.js
@@ -1,8 +1,57 @@
-// const express = require('express');
-// const { db } = require('../server/db');
+const express = require('express');
+const { db } = require('../server/db');
 
-// const donationRouter = express.Router();
+const donationRouter = express.Router();
 
-// Your code below (delete this line when committing):
+donationRouter.get('/', async (req, res) => {
+  try {
+    const donations = await db.query(`
+      SELECT * FROM donation_tracking;
+    `);
+    res.status(200).send(donations);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
 
-// module.exports = donationRouter;
+donationRouter.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const donation = await db.query('SELECT * FROM donation_tracking WHERE donation_id = $(id)', {
+      id,
+    });
+    res.status(200).send(donation);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+donationRouter.put('/:donationID', async (req, res) => {
+  try {
+    const { donationID } = req.params;
+
+    const { businessID, foodBankDonation, reporter, email } = req.body;
+
+    if (!businessID) throw new Error('Business ID is required.');
+    if (!foodBankDonation) throw new Error('Food Bank Donation is required.');
+    if (!reporter) throw new Error('Reporter is required.');
+    if (!email) throw new Error('Email is required.');
+
+    const updatedDonation = await db.query(
+      `UPDATE donation_tracking SET
+      donation_id = $(donationID)
+      ${businessID ? `, business_id = $(businessID) ` : ''}
+      ${foodBankDonation ? `, food_bank_donation = $(foodBankDonation) ` : ''}
+      ${reporter ? `, reporter = $(reporter) ` : ''}
+      ${email ? `, email = $(email) ` : ''}
+      WHERE donation_id = $(donationID)
+      RETURNING *;`,
+      { businessID, donationID, foodBankDonation, reporter, email },
+    );
+    return res.status(200).send(updatedDonation);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+});
+
+module.exports = donationRouter;
