fb4dda5e37dd7031e220fea40c817df8fdc6c8e0
Kainoa Nishida <kanishid@uci.edu>
2023-12-21 18:46:42 -0800

    resolved comments

--- routes/donationRouter.js
+++ routes/donationRouter.js
@@ -28,25 +28,20 @@ donationRouter.get('/:id', async (req, res) => {
 
 donationRouter.put('/:donationID', async (req, res) => {
   try {
-    const { donationID } = req.params;
+    const { donationId } = req.params;
 
-    const { businessID, foodBankDonation, reporter, email } = req.body;
-
-    if (!businessID) throw new Error('Business ID is required.');
-    if (!foodBankDonation) throw new Error('Food Bank Donation is required.');
-    if (!reporter) throw new Error('Reporter is required.');
-    if (!email) throw new Error('Email is required.');
+    const { businessId, foodBankDonation, reporter, email } = req.body;
 
     const updatedDonation = await db.query(
       `UPDATE donation_tracking SET
       donation_id = $(donationID)
-      ${businessID ? `, business_id = $(businessID) ` : ''}
+      ${businessId ? `, business_id = $(businessID) ` : ''}
       ${foodBankDonation ? `, food_bank_donation = $(foodBankDonation) ` : ''}
       ${reporter ? `, reporter = $(reporter) ` : ''}
       ${email ? `, email = $(email) ` : ''}
       WHERE donation_id = $(donationID)
       RETURNING *;`,
-      { businessID, donationID, foodBankDonation, reporter, email },
+      { businessId, donationId, foodBankDonation, reporter, email },
     );
     return res.status(200).send(updatedDonation);
   } catch (err) {
