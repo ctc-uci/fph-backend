2b3688702910081ea794f99c86e626bd4e49a860
joshualipton <jalipton11@gmail.com>
2024-01-09 12:21:09 -0800

    move donation delete and put routes to donationRouter.js

--- routes/donationRouter.js
+++ routes/donationRouter.js
@@ -75,4 +75,68 @@ donationRouter.post('/', async (req, res) => {
   }
 });
 
+donationRouter.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    await db.query('DELETE from donation_tracking WHERE donation_id = $1;', [id]);
+    res.status(200).send('Deleted donation');
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+donationRouter.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      businessId,
+      foodBankDonation,
+      reporter,
+      email,
+      date,
+      cannedDogFoodQuantity,
+      dryDogFoodQuantity,
+      cannedCatFoodQuantity,
+      dryCatFoodQuantity,
+      miscItems,
+      volunteerHours,
+    } = req.body;
+
+    const updatedValue = await db.query(
+      `UPDATE donation_tracking 
+        SET donation_id = $(id)
+        ${businessId ? `, business_id = $(businessId) ` : ''}
+        ${foodBankDonation ? `, food_bank_donation = $(foodBankDonation) ` : ''}
+        ${reporter ? `, reporter = $(reporter) ` : ''}
+        ${email ? `, email = $(email) ` : ''}
+        ${date ? `, date = $(date) ` : ''}
+        ${cannedDogFoodQuantity ? `, canned_dog_food_quantity = $(cannedDogFoodQuantity) ` : ''}
+        ${dryDogFoodQuantity ? `, dry_dog_food_quantity = $(dryDogFoodQuantity) ` : ''}
+        ${cannedCatFoodQuantity ? `, canned_cat_food_quanitty = $(cannedCatFoodQuanitty) ` : ''}
+        ${dryCatFoodQuantity ? `, dry_cat_food_quantity = $(dryCatFoodQuantity) ` : ''}
+        ${miscItems ? `, misc_items = $(miscItems) ` : ''}
+        ${volunteerHours ? `, volunteer_hours = $(volunteerHours) ` : ''}
+        WHERE donation_id = $(id)
+        RETURNING *;`,
+      {
+        id,
+        businessId,
+        foodBankDonation,
+        reporter,
+        email,
+        date,
+        cannedDogFoodQuantity,
+        dryDogFoodQuantity,
+        cannedCatFoodQuantity,
+        dryCatFoodQuantity,
+        miscItems,
+        volunteerHours,
+      },
+    );
+    return res.status(200).send(updatedValue);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+});
+
 module.exports = donationRouter;
